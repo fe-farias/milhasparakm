e3d486790e7bbd6195a7e9825888c8462f686c3c
Felipe Francisco <fefarias97@gmail.com>
2021-09-17 18:18:17 -0300

    Resolução exercício MilhasParaKM

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace milhasparakm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhasdigitada, milhasparakm;
+
+            Console.Write("Entre com a medida (em milhas): ");
+            milhasdigitada = Convert.ToDouble(Console.ReadLine());
+
+            milhasparakm = milhasdigitada * 1.609;
+
+            Console.WriteLine($"{milhasparakm} Km");
+        }
+    }
+}
